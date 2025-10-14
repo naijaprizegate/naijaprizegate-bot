1a77f688cb7d76bed54f4f4fa0a8b2a860045e4f
naijaprizegate <naijaprizegate@gmail.com>
2025-10-14 11:38:30 +0100

    Create base.py

---
+++ base.py
@@ -0,0 +1,11 @@
+# ========================================================
+# base.py
+# ========================================================
+# ===================================================
+# This file defines the declarative Base for SQLAlchemy.
+# Import this Base in both db.py and models.py
+# ===================================================
+
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
