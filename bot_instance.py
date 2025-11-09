f95870441918ebd03c371eef12b68a92b498e885
naijaprizegate <naijaprizegate@gmail.com>
2025-11-09 16:34:41 +0100

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -9,3 +9,5 @@ if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN environment variable is required")
 
 bot = Bot(token=BOT_TOKEN)
+
+BOT_USERNAME = os.getenv("BOT_USERNAME", "NaijaPrizeGateBot") 
