e3745a3603d7411638907ac0a9b5a422fdd6011f
naijaprizegate <naijaprizegate@gmail.com>
2025-11-09 16:22:27 +0100

    Create bot_instance.py

---
+++ bot_instance.py
@@ -0,0 +1,11 @@
+# ===================================================
+# bot_instance.py
+# ===================================================
+import os
+from telegram import Bot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN environment variable is required")
+
+bot = Bot(token=BOT_TOKEN)
