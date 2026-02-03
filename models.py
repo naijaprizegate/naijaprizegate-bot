85e6f85fe7b368be9a4ee0741d3f863741ccb939
naijaprizegate <naijaprizegate@gmail.com>
2026-02-03 12:34:12 +0100

    Update models.py

--- models.py
+++ models.py
@@ -90,7 +90,11 @@ class Play(Base):
 
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     result = Column(Text, nullable=False)
-    created_at = Column(DateTime(timezone=True, server_default=func.now(), nullable=False,))
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
 
     user = relationship("User", back_populates="plays")
 
