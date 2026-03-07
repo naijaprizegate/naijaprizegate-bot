5f4a1d7c00f7fd182792a4eaa914592d99f4ee08
naijaprizegate <naijaprizegate@gmail.com>
2026-03-07 18:14:44 +0100

    Create import_questions.py

---
+++ import_questions.py
@@ -0,0 +1,38 @@
+import json
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="quizdb",
+    user="postgres",
+    password="yourpassword",
+    host="localhost"
+)
+
+cur = conn.cursor()
+
+with open("questions.json", "r") as f:
+    data = json.load(f)
+
+for category, questions in data.items():
+
+    for q in questions:
+
+        cur.execute("""
+            INSERT INTO questions
+            (category, question, option_a, option_b, option_c, option_d, correct_option)
+            VALUES (%s,%s,%s,%s,%s,%s,%s)
+        """, (
+            category,
+            q["question"],
+            q["options"][0],
+            q["options"][1],
+            q["options"][2],
+            q["options"][3],
+            q["answer"]
+        ))
+
+conn.commit()
+cur.close()
+conn.close()
+
+print("Questions imported successfully")
