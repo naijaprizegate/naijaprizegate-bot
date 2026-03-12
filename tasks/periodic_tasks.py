a65695871a1d774b945381d63b74cb06f2b379d4
naijaprizegate <naijaprizegate@gmail.com>
2026-03-12 17:19:02 +0100

    Update periodic_tasks.py

--- tasks/periodic_tasks.py
+++ tasks/periodic_tasks.py
@@ -4,6 +4,7 @@
 """
 Periodic background task manager for:
 - Airtime auto payouts
+- Battle completion checker
 - Sweeper for pending payments
 - Notification retries
 - DB cleanup
@@ -11,7 +12,8 @@ Periodic background task manager for:
 import asyncio
 from logger import logger
 
-from . import sweeper, notifier, cleanup
+from . import sweeper, notifier, cleanup, battle_notifier
+from bot_instance import bot
 
 
 async def start_all_tasks(loop: asyncio.AbstractEventLoop = None) -> list[asyncio.Task]:
@@ -25,8 +27,10 @@ async def start_all_tasks(loop: asyncio.AbstractEventLoop = None) -> list[asynci
         loop.create_task(sweeper.expire_pending_payments_loop(), name="SweeperLoop"),
         loop.create_task(notifier.notifier_loop(), name="AirtimeNotifierLoop"),
         loop.create_task(notifier.retry_failed_notifications_loop(), name="RetryFailedNotificationsLoop"),
+        loop.create_task(battle_notifier.battle_notifier_loop(bot), name="BattleNotifierLoop"),
         loop.create_task(cleanup.cleanup_loop(), name="CleanupLoop"),
     ]
 
     logger.info("🚀 All periodic background tasks are now running")
     return tasks
+
