9fc6f5a7c0e49f35af3d0fbe19f91f938d4b3af1
naijaprizegate <naijaprizegate@gmail.com>
2026-05-07 12:00:55 +0100

    Refactor print statements for university loader tests

--- test_university_loader.py
+++ test_university_loader.py
@@ -1,21 +1,29 @@
 from university_loader import (
     get_university_categories,
-    get_university_subjects,
+    get_university_subjects_by_category,
     get_university_topics,
     load_university_topic_questions,
 )
 
 print(get_university_categories())
 
-print(get_university_subjects("science_foundation"))
+print(
+    get_university_subjects_by_category(
+        "science_foundation"
+    )
+)
 
-print(get_university_topics(
-    "science_foundation",
-    "biology"
-))
+print(
+    get_university_topics(
+        "science_foundation",
+        "general_biology"
+    )
+)
 
-print(load_university_topic_questions(
-    "science_foundation",
-    "biology",
-    "cell_biology"
-))
+print(
+    load_university_topic_questions(
+        "science_foundation",
+        "general_biology",
+        "cell_biology"
+    )
+)
